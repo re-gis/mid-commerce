724f69ff31e2ba08ca9639ba3df882a1e9244890
re-gis <irumvaregisdmc@gmail.com>
2024-08-06 18:10:38 +0200

    [update]: checkout

--- src/order/order.service.ts
+++ src/order/order.service.ts
@@ -1,20 +1,43 @@
 /* eslint-disable prettier/prettier */
-import { Injectable } from '@nestjs/common';
+import { Injectable, InternalServerErrorException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { OrderItem } from 'src/entities/order-item.entity';
 import { Order } from 'src/entities/order.entity';
+import { User } from 'src/entities/user.entity';
 import IResponse from 'src/enums/response.enum';
 import { Repository } from 'typeorm';
 
 @Injectable()
 export class OrderService {
-    @InjectRepository(Order)
-    private readonly orderRepository: Repository<Order>
+  @InjectRepository(Order)
+  private readonly orderRepository: Repository<Order>;
 
-    @InjectRepository(OrderItem)
-    private readonly orderItemRepository: Repository<OrderItem>
-    
-    getMyOrders = ():Promise<IResponse | undefined> => {
+  @InjectRepository(OrderItem)
+  private readonly orderItemRepository: Repository<OrderItem>;
 
+  @InjectRepository(User)
+  private readonly userRepository: Repository<User>;
+
+  getMyOrders = async (u: any): Promise<IResponse | undefined> => {
+    try {
+      const user: User = await this.userRepository.findOne({
+        where: { email: u.email },
+        relations: ['orders', 'orders.items'],
+      });
+
+      if (!user.orders || user.orders.length === 0) {
+        return {
+          status: 200,
+          message: user.orders,
+        };
+      }
+
+      return {
+        status: 200,
+        message: user.orders,
+      };
+    } catch (e) {
+      throw new InternalServerErrorException(e);
     }
+  };
 }
